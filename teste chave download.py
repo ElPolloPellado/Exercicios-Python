bcc44195d28aa8d688cf9a1bfa0d57a1a01ab053
ElPolloPellado <151039254+ElPolloPellado@users.noreply.github.com>
2023-12-06 09:54:56 +0000

    Add files via upload

---
+++ teste chave download.py
@@ -0,0 +1,7 @@
+chave = "c7c80bcf8ff74057a6c1c0f975e5dea17e6a440988f89ae95772aab320de1263672e9a8d74bae941857499037ed1ff012b232f11d2f3089397f2f285d12803bc"
+check = "c7c80bcf8ff74057a6c1c0f975e5dea17e6a440988f89ae95772aab320de1263672e9a8d74bae941857499037ed1ff012b232f11d2f3089397f2f285d12803bc"
+if chave==check:
+    print ("download validado")
+else:
+    print ("download nao validado, nao prosseguir com a instalacao")
+    
